8e62a442c398235c22e7556d2be0e77ad6b7a9b9
Dmitry Sukhikh <china@pozitive.biz>
2014-09-01 00:06:01 +0400

    Recent block

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @promo_articles = Article.promo
-    @recent_articles = Article.not_promo.date.first(6)
+    @recent_articles = Article.not_promo.date.first(9)
   end
 end
