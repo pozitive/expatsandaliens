d1bc72e7873bc31418c87671be7846b2fd2a66c5
Dmitry Sukhikh <china@pozitive.biz>
2014-09-01 23:10:02 +0400

    Add friendly_id to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :articles
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901190548) do
+ActiveRecord::Schema.define(version: 20140901190926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,10 @@ ActiveRecord::Schema.define(version: 20140901190548) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
