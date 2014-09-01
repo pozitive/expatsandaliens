b61604e121f48e849dd05e165887e514557c9f41
Dmitry Sukhikh <china@pozitive.biz>
2014-09-01 23:07:29 +0400

    Add friendly_id to page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901190253) do
+ActiveRecord::Schema.define(version: 20140901190548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 20140901190253) do
     t.string   "keyword"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true, using: :btree
 
 end
