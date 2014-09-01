0dc1743a22ca61a602c8a4aaef88ddf5c0030afa
Dmitry Sukhikh <china@pozitive.biz>
2014-09-01 23:05:37 +0400

    Add friendly_id to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ActiveRecord::Base
   validates_length_of :summary, :keyword, :name, :maximum => 250
   belongs_to :category
+  extend FriendlyId
+  friendly_id :name, use: :slugged
   mount_uploader :image, ImageUploader
+
   scope :promo, -> { where(:promo => true) }
   scope :not_promo, -> { where(:promo => false) }
   scope :date, -> { order('updated_at DESC') }
+
 end
