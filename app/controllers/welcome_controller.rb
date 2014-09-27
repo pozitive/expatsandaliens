df29ab9968aec897bfae0723c867df6413ad1ba7
Dmitry Sukhikh <china@pozitive.biz>
2014-09-28 00:37:44 +0400

    Block all articles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @promo_articles = Article.promo
     @recent_articles = Article.not_promo.date.first(9)
+    @older_articles = Article.not_promo.date.offset(9)
   end
 end
