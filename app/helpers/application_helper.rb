5ae963bd5b6efec94e654b10048dec662d7b033b
Dmitry Sukhikh <china@pozitive.biz>
2014-10-06 09:36:48 +0400

    Fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
     if title.present?
       content_for :title, title
     else
-      content_for?(:title) ? content_for(:title) + ' | Expat and Aliens' : APP_CONFIG['default_title']
+      content_for?(:title) ? content_for(:title) + ' | Expats and Aliens' : APP_CONFIG['default_title']
     end
   end
 
